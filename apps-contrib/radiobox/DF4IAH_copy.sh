2319e9eda631a9d4090a049795bf8a6ecb7d8344
Ulrich Habel <espero7757@gmx.net>
2015-09-26 23:31:56 +0200

    apps-contrib - radiobox - added from 5e36246f685b1f690b5ed3b816de58ee42549880

---
+++ apps-contrib/radiobox/DF4IAH_copy.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+rw; mv /tmp/red_pitaya_top.bit /opt/www/apps/radio_box/fpga.bit ; sync; ro
+
---
+++ apps-contrib/radiobox/DF4IAH_fpga.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+sync
+ro
+cd /
+cat /opt/www/apps/radio_box/fpga.bit >/dev/xdevcfg
+
