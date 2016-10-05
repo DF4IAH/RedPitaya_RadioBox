802594efe56178247b6cd7db620af11e5ca30221
Ulrich Habel <espero7757@gmx.net>
2016-10-05 02:52:10 +0200

    fpga - RadioBox - red_pitaya_vivado.tcl - adding IP generation

--- fpga/red_pitaya_vivado.tcl
+++ fpga/red_pitaya_vivado.tcl
@@ -77,6 +77,8 @@ set_property generic "GITH=160'h$gith" [current_fileset]
 # write checkpoint design
 ################################################################################
 
+generate_target all [get_ips rb_* ac97ctrl_*]
+
 #synth_design -top red_pitaya_top
 synth_design -top red_pitaya_top -flatten_hierarchy none -bufg 16 -keep_equivalent_registers
 
